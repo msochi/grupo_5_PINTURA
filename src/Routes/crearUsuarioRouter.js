5ab44b02d9d43b89d5c29385e339a6729b206101
Marcelo S <marcelo.sochi@gmail.com>
2021-02-09 23:44:55 -0300

    Entrega Sprint 4 y ajustes sprint 3 v2

--- src/Routes/crearUsuarioRouter.js
+++ src/Routes/crearUsuarioRouter.js
@@ -2,25 +2,12 @@ const express = require ('express');
 const router= express.Router ();
 const crearUsuarioController = require ('../Controllers/crearUsuarioController'); // Tengo que importar o requerir el controlador que quiero utilizar para las rutas.
 const multer = require('multer');
-const path= require('path');
 
-// storage sirve para poder guargar las imagenes que sube el usuario junto con requerir multer.
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, "../../public/uploadsAvatars") )
-    },
-    filename: function (req, file, cb) {
-      console.log (req.body.email + Date.now() + path.extname(file.originalname));
-      console.log (req.body);
-      console.log (Date.now());
-      console.log (path.extname(file.originalname));
-      cb(null,req.body.mail + Date.now() + path.extname(file.originalname))
-    }
-  })
-   
-  var upload = multer({ storage: storage })
+const validacionRegistro = require ("../middlewares/validacionRegistro")
+const multerRegistro = require ("../middlewares/multerRegistro");
+
 
 
 router.get ("/", crearUsuarioController.crear); // aca tengo que combinar ruta y controlador. Solo pongo la referencia en el segundo parámetro. Modularizamos la funcionalidad por un laso y la ruta por otro.
-router.post ("/", upload.any(), crearUsuarioController.crearUsuario); //upload.any o upload.single se agrgegan en router como si fuese un middleware para poder capturar las imagenes que suben.
+router.post ("/", multerRegistro.any(), validacionRegistro, crearUsuarioController.crearUsuario); //upload.any o upload.single se agrgegan en router como si fuese un middleware para poder capturar las imagenes que suben.
 module.exports = router;
---
+++ src/middlewares/validacionRegistro.js
@@ -0,0 +1,36 @@
+const {check, body} = require("express-validator"); // Requerimos express-validator para validar el login y verificar que en la creación de cuentas no haya duplicados.
+const fs = require('fs');
+const path= require('path');
+
+let usuarios = fs.readFileSync(path.join(__dirname, '../database/usuarios.json'),'utf8');
+usuarios= JSON.parse(usuarios);
+module.exports =[
+    check("email").isEmail().withMessage("Ingresa un email válido"),
+    body('email')
+      .custom(function(datoDelUsuario) {
+          for(let i = 0; i < usuarios.length; i++) {
+               if(usuarios[i].email == datoDelUsuario) {
+                return false;
+                }
+            }
+                  return true
+    })
+    .withMessage('Email ya registrado con otro Usuario'),
+    check("pass")
+      .isLength ({min:4, max:16}).withMessage("Debe tener un minimo de 4 letras, números o combinación de ambas"),
+    check("dni")
+      .isLength ({min:8, max:8}).withMessage("Debe ingresar un DNI válido de 8 caracteres y sin puntos"),
+    body('dni')
+      .custom(function(datoDelUsuario) {
+        
+        for(let i = 0; i < usuarios.length; i++) {
+              if(usuarios[i].dni == datoDelUsuario) {
+                 return false;
+                }
+            }
+                  return true
+    })
+      .withMessage('DNI ya registrado'),  
+    
+  ]
+  
