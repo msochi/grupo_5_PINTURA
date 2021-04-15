fd036f09ff8b88e9ba1ae3f73ddfa6be9138ee52
Nicolas G Fernandez <nicolas.fernandez@mercadolibre.com>
2021-04-14 22:10:59 -0300

    first

--- public/js/agregadocarro.js
+++ public/js/agregadocarro.js
@@ -60,6 +60,7 @@ var totalOrden = 0;
      totalOrden += val;
  }
  document.getElementById("monto").innerHTML= "$ "+totalOrden;
+ document.getElementById("totalCompra").href= "/checkout?totalOrden="+totalOrden;
  console.log(totalOrden);
 
 
