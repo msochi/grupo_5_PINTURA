71462cec60812cdcb73651bffc3d449074de6ad8
Marcelo S <marcelo.sochi@gmail.com>
2021-04-17 17:46:21 -0300

    agrego CORS

--- src/app.js
+++ src/app.js
@@ -69,7 +69,13 @@ app.use ('/search', searchRouter);
 app.use("/apis/productos", productosRouter);
 app.use("/apis/usuarios", usuariosRouter); // agrego la ruta de la Api para productos.
 
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //app.listen (3000, function () {console.log ("El Servidor esta corriendo")});
 app.listen(port, ()=> console.log ('Server Listen to port: ${port}'));
