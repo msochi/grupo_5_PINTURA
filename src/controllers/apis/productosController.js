d06046f0d79c4d1989f764f045789b4cc94d8401
Marcelo S <marcelo.sochi@gmail.com>
2021-04-17 11:11:36 -0300

    apis

---
+++ src/controllers/apis/productosController.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const db = require('../../database/models')
+
+
+module.exports ={
+   
+ 
+    list: function(req,res){
+       // return res.json (req.body)
+        console.log ('Estoy viendo API detalle de producto')
+    
+         db.Productos.findAll({           
+           include: [
+               {association: 'marcas'},
+               {association: 'tipo'},
+               {association: 'familia'},
+               {association: 'terminacion'},
+               {association: 'subtipo'},
+            ],  
+        }).then(function(productos){
+            console.log(productos)
+            for (let i = 0; i < productos.length; i++) {
+              productos[i].setDataValue("endpoint","/apis/productos/"+productos[i].id)
+                
+            }
+        let respuesta= {
+            meta: {
+                status: 200,
+                total: productos.length,
+                url:"/apis/productos/"
+            },
+            data: productos
+
+        };
+        
+      
+        res.json({respuesta})
+    })
+
+       
+    
+        },
+        detalle: function(req,res){
+            // return res.json (req.body)
+             console.log ('Estoy viendo API detalle de producto')
+         
+              db.Productos.findAll({
+                  where: { id: req.params.id },           
+                include: [
+                    {association: 'marcas'},
+                    {association: 'tipo'},
+                    {association: 'familia'},
+                    {association: 'terminacion'},
+                    {association: 'subtipo'},
+                 ],  
+                }).then(function(productos){
+                 console.log(productos)
+                 for (let i = 0; i < productos.length; i++) {
+                   productos[i].setDataValue("endpoint","/apis/productos/"+productos[i].id)
+                     
+                 }
+             let respuesta= {
+                 meta: {
+                     status: 200,
+                     total: productos.length,
+                     url:"/apis/productos/"
+                 },
+                 data: productos
+     
+             };
+             
+           
+             res.json({respuesta})
+         })
+     
+            
+         
+             },
+
+
+
+    }
